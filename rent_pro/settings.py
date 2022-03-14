05ef7681596796f71fa3e4e36242a0c0672ff386
rayyanInqline786 <69470014+rayyanInqline786@users.noreply.github.com>
2022-03-14 23:09:48 +0500

    Update settings.py

--- rent_pro/settings.py
+++ rent_pro/settings.py
@@ -63,6 +63,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -160,7 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
