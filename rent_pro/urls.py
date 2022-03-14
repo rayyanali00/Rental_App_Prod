891c35b7bf01626967da734106cecf122b9ab501
rayyanInqline786 <69470014+rayyanInqline786@users.noreply.github.com>
2022-03-14 23:40:43 +0500

    Update urls.py

--- rent_pro/urls.py
+++ rent_pro/urls.py
@@ -31,8 +31,8 @@ urlpatterns = [
 
     path('accounts/', include('allauth.urls')),
     path('inbox/notifications/', include(notifications.urls, namespace='notifications')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),    
+    # re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),    
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
