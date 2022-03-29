4fa24e444e283c1f35591a94e9ead4ac1cc6b4b6
rayyanInqline786 <69470014+rayyanInqline786@users.noreply.github.com>
2022-03-29 23:08:04 +0500

    update urls.py

--- rent_pro/urls.py
+++ rent_pro/urls.py
@@ -33,6 +33,4 @@ urlpatterns = [
     path('inbox/notifications/', include(notifications.urls, namespace='notifications')),
     
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
